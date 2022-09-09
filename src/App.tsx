2bb3951c37d394aa71f8897744ccc220d3c98458
Peter Tsivinsky <creatsiv@gmail.com>
2022-09-09 10:44:16 +0300

    feat: setting up & add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Buttons from "./components/Buttons";
+import FieldControls from "./components/FieldControls";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <nav className="navbar navbar-expand navbar-light bg-light">
+        <div className="container">
+          <a href="/" className="navbar-brand">
+            Demo Styleguide
+          </a>
+        </div>
+      </nav>
+      <main className="container pt-4 pb-4">
+        <h1>Components</h1>
+        <hr />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          These components represent common styling scenarios that can exist
+          within a product. It includes:
+          <ul>
+            <li>
+              Custom CSS styles written in{" "}
+              <a href="https://sass-lang.com/">sass</a>
+            </li>
+            <li>
+              Global CSS resets provided by{" "}
+              <a href="https://getbootstrap.com/docs/5.0/content/reboot/">
+                Bootstrap reboot
+              </a>
+            </li>
+            <li>
+              Layout and positioning styles provided by{" "}
+              <a href="https://getbootstrap.com/docs/5.0/utilities/spacing/">
+                Bootstrap spacing and grid
+              </a>
+            </li>
+          </ul>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Buttons />
+        <FieldControls />
+      </main>
+    </>
   );
 }
 
